7f69c2321679c0a64bbfe0f7e884b232f0d4f447
unlikeplatypus <43973727+unlikeplatypus@users.noreply.github.com>
2019-11-12 14:12:03 -0700

    Model Folder and LMS Objects
    Created a Models folder. Created objects: CanvasItem, Assignment, Course, and User.

---
+++ Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOM
+{
+    class Course : CanvasItem
+    {
+        [JsonProperty(PropertyName = "course_code")]
+        public string CourseCode { get; set; }
+
+        [JsonProperty(PropertyName = "enrollment_term_id")]
+        public int EnrollmentTermId { get; set; }
+
+        public Assignment[] Assignments { get; set; }
+
+        public void ParseAssignments(User user, String domain)
+        {
+                string assignmentURL = $"https://{domain}/api/v1/courses/{Id}/assignments?access_token={user.Token};per_page=100";
+                var restClient2 = new RestClient(assignmentURL);
+                var request2 = new RestRequest(Method.GET);
+                var response2 = restClient2.Execute<List<Assignment>>(request2);
+                var upcomingAssignments = response2.Data.Where(duedate => duedate.Due_at >= DateTime.Now).OrderBy(date => date.Due_at);
+                Assignments = upcomingAssignments.ToArray();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,38 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOM
+{
+    class User
+    {
+        public string Username { get; set; }
+        public string Passoword { get; set; }
+        public string Token { get; private set; }
+
+        public Course[] Courses { get; set; }
+
+        public User(String token, string domain)
+        {
+            Token = token;
+            ParseCourses(domain);
+        }
+
+        public void ParseCourses(String domain)
+        {
+            string url = $"https://{domain}/api/v1/courses.json?access_token={Token};per_page=100";
+            var restClient = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            var response = restClient.Execute<List<Course>>(request);
+            var currentClassList = response.Data.Where(thingy => thingy.EnrollmentTermId == 13591);
+            Courses = currentClassList.ToArray();
+            foreach (Course c in Courses)
+            {
+                c.ParseAssignments(this, domain);
+            }
+        }
+    }
+}
